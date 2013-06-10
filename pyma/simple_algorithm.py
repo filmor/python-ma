bacfea55ea179b06f6a53917ca6727a6ca3e7018
Benedikt Sauer <filmor@gmail.com>
2013-06-10 17:35:11 +0200

    Implemented matching of eigenvectors.

--- pyma/simple_algorithm.py
+++ pyma/simple_algorithm.py
@@ -2,32 +2,69 @@ import numpy as np
 import numpy.linalg as la
 import pandas as pd
 
-def gevp(B):
+import operator
+def max_index(iterable):
+    return max(enumerate(iterable), key=operator.itemgetter(1))
+
+def solve_gevp(B, A=None):
     """Solve the generalised eigenvalue problem using the Cholesky
     decomposition."""
     L = la.cholesky(B)
     L_inv = la.inv(L)
     L_inv_t = L_inv.transpose()
-    def gevp(A):
+
+    def solve_gevp(A):
         A_prime = L_inv * A * L_inv_t
         return la.eigh(A_prime)
-    return gevp
 
-def gevp_gen(a, t_0):
+    if A is None:
+        return solve_gevp
+    else:
+        return solve_gevp(A)
+
+
+def solve_gevp_gen(a, t_0):
     """Generator that returns the eigenvalues for t_0 -> t
        where t is in (t_0, t_max]."""
     try:
-        f = gevp(a[t_0])
+        f = solve_gevp(a[t_0])
     except la.LinAlgError:
         return
+
+    eigenvectors = None
+    count = 0
     
     for j in range(t_0 + 1, 32):
         try:
-            eigenvalues, eigenvectors = f(a[j])
-            # TODO: Use the eigenvectors (index 1) to match the eigenvalues
-            yield eigenvalues
+            eigenvalues, new_eigenvectors = f(a[j])
+
+            if eigenvectors is None:
+                eigenvectors = new_eigenvectors
+            else:
+                dot_products = [np.dot(e, eigenvectors) for e in
+                        new_eigenvectors.transpose()]
+
+                perm = [m.argmax() for m in dot_products]
+
+                eigenvectors = eigenvectors + new_eigenvectors[:,perm]
+                eigenvalues = eigenvalues[:,perm]
+                
+            count += 1
+
+            yield eigenvalues, eigenvectors / count
+
         except la.LinAlgError:
             pass
+
         
 def calculate_gevp(m):
-    return pd.concat({i: pd.DataFrame(list(gevp_gen(m, i))) for i in range(32)})
+    res_values = {}
+    for i in range(32):
+        ev = []
+        for eigenvalues, _eigenvectors in solve_gevp_gen(m, i):
+            ev.append(eigenvalues)
+
+        if len(ev):
+            res_values[i] = pd.DataFrame(ev)
+
+    return pd.concat(res_values)
