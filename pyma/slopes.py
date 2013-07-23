9c798b9ef97ab1399ace3a5694f0ea77dbd1bc8a
Benedikt Sauer <filmor@gmail.com>
2013-07-23 16:06:28 +0200

    Added chi2 calculation and another fit-method.

--- pyma/slopes.py
+++ pyma/slopes.py
@@ -35,17 +35,62 @@ def get_slope_simple(data, error=None, eps=0.05):
 
 # def get_slope_as_exp
 
-from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit, leastsq
+
+def _func(p, x):
+    return p[0] * np.exp(-p[1] * x)
+
+def _d_func(p, x, *args):
+    v = np.exp(-p[1] * x)
+    return [v, -p[0] * x * v]
+
+def get_slope_exp_fit_2(data, error=None, range=slice(1,7)):
+    res = lambda p, x, y: _func(p, x) - y
+    
+    if error is not None:
+        res1 = lambda p, x, y: (_func(p, x) - y) / error[range].data
+    else:
+        res1 = res
+
+    def residuals(p, x, y):
+        if p[1] < 0:
+            return 1e6
+        else:
+            return res1(p, x, y)
+
+    args = (data[range].index.data, data[range].data)
+    p0 = [0.4, 0.4]
+    
+    (popt, pcov, infodict, errmsg, ier) = \
+        leastsq(residuals, p0,
+            args=args,
+            Dfun=_d_func,
+            col_deriv=1,
+            full_output=1
+            )
+
+    chi2 = np.sum(res1(popt, *args) ** 2) / (len(args[1]) - len(p0))
+    pcov *= chi2
+
+    if ier not in [1, 2, 3, 4]:
+        raise RuntimeError("Optimal solution not found: %s" % errmsg)
+
+    return popt, pcov, chi2
 
 def get_slope_exp_fit(data, error=None, range=slice(1,7)):
     error = pd.Series(1, index=data.index) if error is None else error
 
-    fit_result, cov_matrix = curve_fit(
-            lambda x, A, B: A * np.exp(-B * x),
-            data[range].index.data,
-            data[range].data,
+    args = (data[range].index.data, data[range].data)
+
+    popt, pcov = curve_fit(
+            lambda x, A, B: _func([A, B], x),
+            args[0],
+            args[1],
             np.array([0.4, 0.4]),
             sigma = error[range].data
         )
 
-    return fit_result, cov_matrix
+    chi2 = np.sum((_func(popt, args[0]) - args[1]) ** 2) / (len(args[1]) -
+        len(popt))
+
+    return popt, pcov, chi2
