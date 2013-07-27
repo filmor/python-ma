83c8db0da9eee235fe9899c315c7076f2207411e
Benedikt Sauer <filmor@gmail.com>
2013-07-27 15:09:20 +0200

    Initial ESPRIT implementation.

---
+++ pyma/esprit.py
@@ -0,0 +1,14 @@
+from .simple_algorithms import scipy_solver
+import scipy.linalg as la
+
+def _pe_single(A):
+    U, S, V_t = la.svd(A)
+    _, _, V_U_t = la.svd(U)
+    _, _, V_V_t = la.svd(V_t.transpose())
+    return V_U_t * S * V_V_t.transpose()
+
+def pro_esprit(B, A):
+    B_ = _pe_single(B)
+    A_ = _pe_single(A)
+    return scipy_solver(B, A)
+
