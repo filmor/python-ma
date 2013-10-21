6e495e5e6c8bebf517df2ecb13094a48fc84ed2f
Benedikt Sauer <filmor@gmail.com>
2013-10-21 15:09:02 +0200

    Fixed mass plot output.

--- pyma/masses.py
+++ pyma/masses.py
@@ -25,3 +25,46 @@ def calc_masses(gevs, method="cosh"):
     return gevs.groupby(level=[0, 1],
             group_keys=False).apply(calc_masses_cosh).reorder_levels([1,2,0])
 
+def mass_plot(data, t_0, ev, ax, label, calc_masses=calc_masses_cosh):
+    masses = data.xs(t_0, level=1)[ev].groupby(level=0, group_keys=False)\
+            .apply(calc_masses)
+    
+    yerr = masses.ix[1:].std(level=1)
+    y = masses.ix[0]
+    
+    x = y.index
+        
+    ax.errorbar(x, y, fmt=".", yerr=yerr, label=label)
+    
+    #sum_of_weights = (1 / yerr ** 2).sum()
+    #avg = (y / yerr ** 2).sum() / sum_of_weights
+    # std = (((y - avg) / yerr)**2).sum() / sum_of_weights
+    
+    #precision = str(int(ceil(-math.log10(std))))
+    #print(("'Fit': %." + precision + "f +- %." + precision + "f for %s") % (avg, std, label))
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import sys
+import pandas as pd
+
+class PgfPlotter(object):
+    def __init__(self, data_file):
+        self._fig = None
+        self._ax = None
+        self._store = pd.HDFStore(data_file, "r")
+        mpl.use("pgf", warn=False)
+
+    def start(self):
+        self._fig = plt.figure(figsize=(5, 4))
+        self._ax = self._fig.gca()
+        self._ax.set_ylim((0, 1))
+
+    def plot(self, data_set, t_0, ev, label):
+        data = self._store[data_set]
+        mass_plot(data, t_0, ev, self._ax, label)
+
+    def end(self):
+        self._ax.legend()
+        self._fig.savefig(sys.stdout, format="pgf")
+
