e3f20f2c9cb47ea759edda973bc835bde3f48f3c
Benedikt Sauer <filmor@gmail.com>
2013-10-25 21:33:39 +0200

    Split filter and calculation.

--- pyma/generate_gevs.py
+++ pyma/generate_gevs.py
@@ -3,65 +3,99 @@ from os import path
 
 from multiprocessing import Pool, get_logger, cpu_count
 import pandas as pd
+import numpy as np
 import traceback
+
+def import_(func, params):
+    from importlib import import_module
+    from functools import partial
+
+    if func is None:
+        return lambda data: data
+
+    mod, _, func = func.rpartition(".")
+    try:
+        func = getattr(import_module(mod), func)
+    except:
+        raise RuntimeError("Could not load %s from %s" % (func, mod))
+
+    return partial(func, **params)
+
 
 def main(args):
     try:
-        return main_(*args)
+        if args[0] == "filter":
+            return run_filter(*args[1:])
+        elif args[0] == "algo":
+            return run_algo(*args[1:])
     except:
         get_logger().error(traceback.format_exc())
         return False
 
-def main_(inp, fil, alg, output_file, overwrite=False):
-    def import_(func, params):
-        from importlib import import_module
-        from functools import partial
+def check_key(filename, key):
+    try:
+        store = pd.HDFStore(filename, "r")
+        result = key in store
+        store.close()
+    except IOError:
+        return False
+    return result
 
-        if func is None:
-            return lambda data: data
-
-        mod, _, func = func.rpartition(".")
-        try:
-            func = getattr(import_module(mod), func)
-        except:
-            raise RuntimeError("Could not load %s from %s" % (func, mod))
-
-        return partial(func, **params)
-
-    from test_algo import load_data, calculate, write_data
-    
+def run_filter(inp, fil, args):
+    from test_algo import load_data, write_data
     input_name, input_config = inp
     filter_name, filter_func, filter_params = fil
-    algo_name, algo_func, algo_params = alg
-    key = "/".join(["", input_name, algo_name, filter_name])
 
-    if not overwrite:
-        try:
-            pd.read_hdf(output_file, key)
-            print("Skipping %s" % key)
-            # TODO
-            return True
-        except (KeyError, IOError):
-            pass
+    key = "/".join(["/data", input_name, filter_name])
+    
+    if not args.overwrite and check_key(args.output_file, key):
+        print("Skipping %s" % key)
+        return True
+
+    print("Running %s ... " % key)
+    data = load_data(input_config["filename"], input_config["key"])
+    filter = import_(filter_func, filter_params)
+
+    result = np.array([
+        filter(t) for t in data
+        ])
+
+    print("Writing filtered data %s to %s" % (key, args.output_file))
+    write_data(args.output_file, key, result)
+
+    print("Done with %s" % key)
+    return True
+
+
+def run_algo(inp, fil, alg, args):
+    from test_algo import load_data, calculate
+    
+    input_name, _ = inp
+    filter_name, _, _ = fil
+
+    input_key = "/".join(["/data", input_name, filter_name])
+
+    algo_name, algo_func, algo_params = alg
+    key = "/".join(["/gevs", input_name, algo_name, filter_name])
+
+    if not args.overwrite and check_key(args.output_file, key):
+        print("Skipping %s" % key)
+        return True
 
     print("Running %s ... " % key)
 
-    data = load_data(input_config["filename"], input_config["key"])
-    filter = import_(filter_func, filter_params)
+    data = load_data(args.output_file, input_key) 
     algo = import_(algo_func, algo_params)
 
-    res = calculate(data, algo, filter)
+    res = calculate(data, algo)
 
     print("Writing the gevs of %s to %s" % (key, output_file))
     res.to_hdf(output_file, key)
 
     print("Done with %s" % key)
 
-    print("Starting analysis on %s" % key)
-    # TODO, from test_algo
-    ev_count = int(input_config["ev_count"])
-
     return True
+
 
 def load_config(config_dir, name):
     try:
@@ -89,13 +123,20 @@ def unpack(config):
         else:
             yield (name, c.get("func", None), {})
 
-def config_iter(config, args=()):
+def filter_iter(config, args):
+    for inp in config["input"].items():
+        if inp[0].startswith("_"):
+            continue
+        for f in unpack(config["filter"]):
+            yield ("filter", inp, f, args)
+
+def algo_iter(config, args):
     for inp in config["input"].items():
         if inp[0].startswith("_"):
             continue
         for f in unpack(config["filter"]):
             for a in unpack(config["algorithm"]):
-                yield (inp, f, a) + args
+                yield ("algo", inp, f, a, args)
 
 if __name__ == '__main__':
     import argparse
@@ -120,12 +161,12 @@ if __name__ == '__main__':
             }
 
     pool = Pool(processes=args.processes)
-    result = pool.map(main, config_iter(config, (output_file, overwrite)))
+    result = pool.map(main, filter_iter(config, args))
+    result += pool.map(main, algo_iter(config, args))
 
     pool.close()
     pool.join()
 
     import sys
     sys.exit(0 if (all(result)) else -1)
-
 
